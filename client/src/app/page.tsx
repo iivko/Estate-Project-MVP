32be3fef44599fc7d1251bac90233a85169bf83b
iivko <iiivkoo11@gmail.com>
2025-10-16 18:24:31 +0300

    chore: setup shadcn ui, prettier and eslint

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
       <div>
-          <h1 className="text-6xl">Hom!!!e</h1>
+          <h1 className="text-6xl">Estamaniana</h1>
       </div>
   );
 }
